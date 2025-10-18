ee002fa1338b6fa50da881d74ebe8315cfbd8030
VedantKedia2002 <vkedia365@gmail.com>
2025-10-18 19:56:25 +0530

    adding project files

---
+++ functions/os_ops.py
@@ -0,0 +1,20 @@
+import os
+import subprocess as sp
+
+paths = {
+    'notepad': r"C:\Windows\System32\notepad.exe",
+    'calculator': r"C:\Windows\System32\calc.exe"
+}
+
+def open_notepad():
+    import os
+    os.startfile(paths['notepad'])
+
+def open_camera():
+    sp.run('start microsoft.windows.camera:', shell=True)
+
+def open_cmd():
+    os.system('start cmd')
+
+def open_calculator():
+    sp.Popen(paths['calculator'])
